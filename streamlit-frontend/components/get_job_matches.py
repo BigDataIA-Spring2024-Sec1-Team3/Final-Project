3dee23781fbbe327f11dc54952fdf636a14c94fd
Ritesh <choudhary.rit@northeastern.edu>
2024-04-23 01:50:46 -0400

    Added modal

--- streamlit-frontend/components/get_job_matches.py
+++ streamlit-frontend/components/get_job_matches.py
@@ -1,4 +1,4 @@
-
+from streamlit_modal import Modal
 import streamlit as st
 import configparser
 import requests
@@ -39,17 +39,26 @@ def show_find_jobs():
         st.button("Get Job Matches")
         view_button = st.button("View Resume")
 
+        modal = Modal(
+            key="resume_modal",
+            title="Resume",
+            padding=50,
+            max_width=1000
+        )
+
         if view_button:
-            access_token = st.session_state['access_token']
-            headers = {"Authorization": f"Bearer {access_token}"}
-            base_url = config['APIs']['base_url_auth']
-            url = base_url + f"userRoutes/getResume/?file_name=" + selected_resume
-            response = requests.get(
-                url, headers=headers)
-            print(response)
-            if response.status_code == 200:
-                st.success("Resume fetched successfully.")
-                st.markdown(
-                    f'<iframe src="{url}" width="700" height="1000"></iframe>', unsafe_allow_html=True)
-            else:
-                st.error("Failed to fetch the resume.")
+            modal.open()
+
+        if modal.is_open():
+            with modal.container():
+                access_token = st.session_state['access_token']
+                headers = {"Authorization": f"Bearer {access_token}"}
+                base_url = config['APIs']['base_url_auth']
+                url = base_url + f"userRoutes/getResume/?file_name=" + selected_resume
+                response = requests.get(
+                    url, headers=headers)
+                if response.status_code == 200:
+                    st.markdown(
+                        f'<iframe src="{url}" width="1000" height="1000"></iframe>', unsafe_allow_html=True)
+                else:
+                    st.error("Failed to fetch the resume.")

b243ff822c95338c003250461c1beb759714440d
rachanaKeshav <keshav.r@northeastern.edu>
2024-04-25 16:10:31 -0400

    streamlit ui for job recommendations and api integration

--- fastapi-backend/routes/userRoutes.py
+++ fastapi-backend/routes/userRoutes.py
@@ -6,6 +6,13 @@ import jwt
 from connections import aws_connection, mongo_connection
 from botocore.exceptions import NoCredentialsError, ClientError
 from starlette.responses import StreamingResponse
+from sentence_transformers import SentenceTransformer
+import torch
+from pinecone import Pinecone
+from connections import aws_connection, pinecone_connection, snowflake_connection
+import os
+from PyPDF2 import PdfReader
+import re
 
 router = APIRouter()
 
@@ -77,6 +84,139 @@ def upload_to_s3(file, s3, bucket_name, file_name):
         return False, "File not found"
     except NoCredentialsError:
         return False, "Credentials not found"
+    
+def pdf_txt_extraction(file_name):
+    '''
+    Function to:
+        - Extract content from resume PDF files stored in AWS S3
+        - Store the extracted text files back to S3
+    '''
+    try:
+        s3_client, bucket_name = aws_connection()
+
+        # Folder containing PDF files
+        resume_files_folder = config['s3-bucket']['resumes_folder_name']
+
+        # Folder to store extracted text files
+        txt_file_folder = config['s3-bucket']['text_folder_name']
+        
+        input_file_key=resume_files_folder+file_name
+        s3_client.download_file(bucket_name, input_file_key, file_name)
+
+        # Extract text from the downloaded PDF file
+        text = ''
+        with open(file_name, 'rb') as file:
+            reader = PdfReader(file)
+            for page in reader.pages:
+                extracted_text = page.extract_text() or ''
+                # Remove extra spaces and clean up text
+                cleaned_text = re.sub(r'\s+', ' ', extracted_text).strip()
+                text += cleaned_text + ' '
+
+        # Join non-empty sections
+        text = ' '.join(filter(None, text.split(' ')))
+
+        # Delete the temporary downloaded file (pdf)
+        os.remove(file_name)
+
+        # Writing file to S3 bucket
+        file_name_wo_extension = os.path.splitext(os.path.basename(file_name))[0]
+        output_file_path = file_name_wo_extension + '.txt'
+        with open(output_file_path, 'w', encoding="utf-8") as file:
+            file.write(text)
+
+        # Upload the file to S3
+        output_file_key = txt_file_folder + output_file_path
+        response = s3_client.upload_file(output_file_path, bucket_name, output_file_key)
+
+        # Delete the temporary downloaded file (txt)
+        os.remove(output_file_path)
+
+        return response
+    except Exception as e:
+        print("Exception in pdf_txt_extraction function: ", e)
+        return None
+
+    
+def fetch_text_data(file_name):
+    try:
+        s3_client, bucket_name = aws_connection()
+
+        # Specify the key path of the file you want to retrieve
+        key_path = config['s3-bucket']['text_folder_name'] + file_name
+
+        # Retrieve the object from S3
+        response = s3_client.get_object(Bucket=bucket_name, Key=key_path)
+        
+        # Read the contents of the object and decode it
+        text = response['Body'].read().decode('utf-8')
+
+        return text
+    
+    except Exception as e:
+        print("Error fetching text data:", e)
+        return None
+
+def getJobDetails(job_id):
+    try:
+        conn, table = snowflake_connection()
+        fetch_query = f"SELECT job_id, job_title, company, job_location, source, url, date_posted, description FROM {table} WHERE job_id = '{job_id}';"
+
+        cur = conn.cursor()
+
+        cur.execute(fetch_query)
+
+        row = cur.fetchone()
+        if row:
+            return {
+            'job_id': row[0],
+            'job_title': row[1],
+            'company': row[2],
+            'job_location': row[3],
+            'source': row[4],
+            'url': row[5],
+            'date_posted': row[6]
+        }
+        else:
+            return None
+
+    except Exception as e:
+        print("Exception in fetch_data_from_snowflake function", e)
+        return ''
+    
+
+def fetch_from_pinecone(file_name):
+    '''
+    function to fetch data from pinecone
+    '''
+    try:
+        # Pinecone
+        pinecone_api_key, index_name = pinecone_connection()
+        pinecone = Pinecone(api_key=pinecone_api_key)
+
+        # fetching data from pinecone
+        index = pinecone.Index(name=index_name)
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if device != 'cuda':
+            model = SentenceTransformer('all-MiniLM-L6-v2', device=device)
+
+        query = fetch_text_data(file_name)
+        xq = model.encode(query).tolist()
+        xc = index.query(vector=xq, top_k=10, include_metadata=True)
+
+        recommendations=[]
+        for match in xc['matches']:
+            score = match['score']
+            job_id = match['id']
+            jobDetails = getJobDetails(job_id)
+            recommendations.append({"score": score, "jobDetails": jobDetails})
+        
+        return recommendations
+
+        # fetching data from pinecone namespace
+    except Exception as e:
+        print("Exception: ", e)
+        return None
 
 # Route to handle multiple file uploads
 @router.post("/upload/")
@@ -98,6 +238,7 @@ async def upload_files_to_s3(files: List[UploadFile] = File(...), current_user: 
             s3_file_url = "s3://" + bucket_name + "/" + file_name
             if success:
                 if mapUserAndFile(current_user, file.filename):
+                    pdf_txt_extraction(file.filename)
                     uploaded_files.append(
                         {"file_name": file.filename, "file_location": s3_file_url, "status": "File uploaded successfully"})
             else:
@@ -110,10 +251,6 @@ async def upload_files_to_s3(files: List[UploadFile] = File(...), current_user: 
         for file in files:
             file.file.close()
 
-
-@router.get("/getJobMatches/")
-async def get_job_matches(jobs: List[str], current_user: dict = Depends(get_current_user)):
-    print(jobs)
 
 @router.get("/files/", response_model=List[str])
 async def get_files(current_user: str = Depends(get_current_user)):
@@ -129,12 +266,10 @@ async def get_files(current_user: str = Depends(get_current_user)):
 
 
 @router.get("/getResume/")
-async def get_resume(file_name: str):
+async def get_resume(file_name: str, current_user: dict = Depends(get_current_user)):
     s3_client, bucket_name = aws_connection()
     resumes_folder_name = config['s3-bucket']['resumes_folder_name']
     full_path = resumes_folder_name + file_name
-    print(f"Attempting to fetch file at path: {full_path}")
-
     try:
         response = s3_client.get_object(Bucket=bucket_name, Key=full_path)
         return StreamingResponse(response['Body'], media_type='application/pdf')
@@ -143,4 +278,19 @@ async def get_resume(file_name: str):
         raise HTTPException(status_code=404, detail="File not found")
     except Exception as e:
         print("An error occurred: " + str(e))
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/getJobRecommendations")
+async def get_job_recommendations(file_name: str, current_user: dict = Depends(get_current_user)):
+    try:
+        # Fetch job recommendations
+        file_name=file_name.replace(".pdf",".txt")
+        recommendations = fetch_from_pinecone(file_name)
+        
+        if recommendations:
+            return recommendations
+        else:
+            raise HTTPException(status_code=404, detail="No job recommendations found for the given file.")
+    
+    except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
